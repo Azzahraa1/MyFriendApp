1c09f1e87b4dbd80e2353460725b3b4b1d1dedd2
Azzahra1 <fatimahazzahra4121@gmail.com>
2023-03-28 08:32:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myfriendapp/AppDataBase.kt
@@ -0,0 +1,31 @@
+package com.example.myfriendapp
+
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [MyFriend::class], version = 1)
+abstract  class AppDataBase: RoomDatabase() {
+    abstract fun myfriendDao(): MyFriendDao
+
+    companion object{
+        var INSTANCE:AppDataBase? = null
+        fun getAppDataBase(context: Context):AppDataBase? {
+            if (INSTANCE == null) {
+                synchronized(AppDataBase::class) {
+                    INSTANCE =
+
+                        Room.databaseBuilder(
+                            context.applicationContext,
+                            AppDataBase::class.java, "MyFriendAppDB"
+                        ).build()
+                }
+            }
+            return INSTANCE
+        }
+    fun destroyDataBase(){
+        INSTANCE = null
+    }}
+    }
